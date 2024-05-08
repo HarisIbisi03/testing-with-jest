9a7a0acbd49463457d2b7bbda62adb77e22b3893
HarisIbisi03 <haris2013@hotmail.se>
2024-05-08 20:21:48 +0200

    Fixade ett litet fel i browser.test.js

--- tests/browser.test.js
+++ tests/browser.test.js
@@ -2,7 +2,7 @@ const { Builder, By, until } = require("selenium-webdriver");
 require("geckodriver");
 
 const fileUnderTest =
-  "file://" + __dirname.replace(/ /g, "%20") + "/../dist/index.html";
+  "file://" + __dirname.replace(/ /g, "%20") + "/../src/index.html";
 const defaultTimeout = 10000;
 let driver;
 jest.setTimeout(1000 * 60 * 5); // 5 minuter
